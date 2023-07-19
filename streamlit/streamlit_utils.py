f2518344eb67d63b214537370c95d84b89093b91
akin-aroge <aroogzbaba1@gmail.com>
2023-07-19 07:26:00 -0700

    clean imports

--- streamlit/streamlit_utils.py
+++ streamlit/streamlit_utils.py
@@ -6,7 +6,7 @@ import altair as alt
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sys.path.append('..')
+sys.path.append('../..')
 from src import inference as inf
 from src import feature_eng, data_proc
 
